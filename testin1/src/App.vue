bc1b4e4fb414415d429491ec8690ea1c161c0329
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-14 23:45:59 +0500

    Getting footer component on display

--- testin1/src/App.vue
+++ testin1/src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <app-header>
 
-  </app-header>
+<div>
+
+  <app-header></app-header>
+
+  <app-footer></app-footer>
+
+</div>
+
 
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components:{
-    'app-header':Header
+    'app-header':Header,
+    'app-footer':Footer
   },
   data(){
     return{
---
+++ testin1/src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <footer>
+        <h5>{{copyright}}</h5>
+    </footer>
+
+
+</template>
+
+<script>
+
+
+export default {
+  data(){
+      return{
+          copyright:"Copyright of AbdulMalik Shareef Studio &copy;"
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+footer{
+    background: #222;
+    padding:6px;
+}
+
+h5{
+    color:lightgreen;
+    text-align:center;
+}
+
+
+</style>
