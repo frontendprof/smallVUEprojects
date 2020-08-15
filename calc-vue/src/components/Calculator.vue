2be1804ce64993e17451dde18d28514b7b3eed88
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-15 19:29:42 +0500

    Setup calc project

---
+++ calc-vue/src/components/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h3>Assalaamu aleykom</h3>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
