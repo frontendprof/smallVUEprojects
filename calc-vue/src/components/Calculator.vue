94f790d5457275950203db442538936ff7adb570
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-15 21:01:36 +0500

    unto css of calc

--- calc-vue/src/components/Calculator.vue
+++ calc-vue/src/components/Calculator.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="hello">
-    <h3>Assalaamu aleykom</h3>
+  <div class="calc">
+    <div class="display">34343434</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn oper">&divide;</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn oper">&times;</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn oper">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn oper">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
     
   </div>
 </template>
@@ -12,8 +31,31 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calc {
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(50px,auto);
+  font-size: 1.5rem;
+  text-align: center;
+  color:rgb(4, 4, 185);
+  background-color: rgb(253, 223, 125);
+}
+
+.display{
+  grid-column: 1/5;
+  background-color: rgb(157, 167, 219);
+  color:#fff;
+  font-size: 40px;
+  border:3px solid red;
+}
+
+.zero{
+  grid-column:1/3;
+}
+
+.btn{
+  border:3px solid #fff;
+  padding: 3px;
 }
 
 </style>
