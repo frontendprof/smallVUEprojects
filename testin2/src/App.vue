c39c6f2c0ad5e9b3fa8f844c3edd5a93ae258687
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-21 13:05:01 +0500

    Testin ...

---
+++ testin2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <form-helper>
+      <h2 slot="title">I am the slot title</h2>
+      <h3 slot="sec">I am the second one, huh?</h3>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from './components/formHelper.vue'
+
+export default {
+  components: {
+    "form-helper":formHelper
+  },
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ testin2/src/components/formHelper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <slot name="title"></slot>
+    <h1>I am the form helper</h1>
+    <slot name="sec"></slot>
+
+   
+  </div>
+</template>
+
+<script>
+export default {
+  components:{
+
+  },
+  data(){
+    return{
+
+    }
+  },
+
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
